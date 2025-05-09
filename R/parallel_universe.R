aa41f1c5b36148bb8e2d9262a8dc8fc7342a361f
Jane Lange <langeja@ohsu.edu>
2025-05-09 00:07:29 -0700

    Jane's updates

--- R/parallel_universe.R
+++ R/parallel_universe.R
@@ -1,28 +1,40 @@
-
-#-------------------------------------------
-# This function extracts rate matrices and cancer sites based on OMST and LMST specifications.
-#-------------------------------------------
-get_rates_list <- function(the_omst, the_lmst, all_meta_data_fits, all_fits, the_cancer_site) {
-  the_indices <- all_meta_data_fits %>%
-    filter(OMST %in% the_omst, LMST %in% the_lmst, cancer_site %in% the_cancer_site) %>%
+#' Extract rate matrices and cancer sites based on OMST and LMST specifications.
+#' 
+#' @param the_omsts vector of overall mean sojourn times for each of the cancer specified cancer sites
+#' @param the_lmsts vector of late mean sojourn times for each of the cancer specified cancer sites
+#' @param all_meta_data dataframe that indexes the rate matrices corresponding to specified OMST and LMST entries
+#' @param all_rates array of rate matrices corresponding to specified OMST and LMST entries
+#' @return A list with two items: 
+#'           1) A list of rate matrices that correspond to the selected OMST and LMST specifications for each the selected cancer sites. 
+#'           2) vector of the cancer sites  
+#' @export
+#' @import purrr 
+get_filtered_rates <- function(the_omsts, the_lmsts, all_meta_data, all_rates, the_cancer_sites) {
+  the_indices <- all_meta_data %>%
+    filter(OMST %in% the_omsts, LMST %in% the_lmsts, cancer_site %in% the_cancer_sites) %>%
     select("index")
  
   ###########################
-  rates_list <- lapply(all_fits[unlist(the_indices)], "[[", "rate.matrix")
-  #For arrays: 
-  # rates_list <- all_fits[,,the_indices]
+  #rates_list <- lapply(all_fits[unlist(the_indices)], "[[", "rate.matrix")
+  #Extract the rate matrices corresponding to the specified OMSTs and LMSTs 
+ #browser()
+   rates_list <- purrr::array_branch(all_rates[,,unlist(the_indices)],3)
+  
   ###########################
   
-  cancer_sites <- all_meta_data_fits %>%
-    filter(OMST %in% the_omst, LMST %in% the_lmst, cancer_site %in% the_cancer_site) %>%
+  cancer_sites <- all_meta_data %>%
+    filter(OMST %in% the_omsts, LMST %in% the_lmsts, cancer_site %in% the_cancer_sites) %>%
     select("cancer_site")
-  
-  return(list(rates_list = rates_list, cancer_sites = cancer_sites))
+
+  return(list(rates_list = rates_list, cancer_sites = cancer_sites$cancer_site))
 }
 
-#-------------------------------------------
-# Function to initialize the state based on the rate matrix and a given time a1
-#-------------------------------------------
+#' Get the initial natural history state based on the rate matrix and starting age, conditional on no clinical diagnoses before starting age.
+#' 
+#' @param rate.matrix the rate matrix used to simulate cancer natural history
+#' @param a1 starting age at first screen 
+#' @return the initial state
+#' @export
 get_init <- function(rate.matrix, a1) {
   k <- dim(rate.matrix)[1]
   init <- numeric(k)
@@ -69,7 +81,8 @@ sim_individual_MCED<-function( ID,
 
   ### get the screening times
   screen_times = seq((starting_age),(num_screens + starting_age-1), by=screen_interval)
-    
+  
+ 
   result <- sim_multiple_cancer_indiv(ID = ID,
                                         cancer_sites = cancer_sites,
                                         rate_matrices = rates_list,
@@ -87,7 +100,9 @@ sim_individual_MCED<-function( ID,
   #                                         result$cancer_site,MoreArgs=list(ovarian_survival_dist))
   # 
   # result$arm = arm
-  # result$censoring_time=censoringtime
+#  browser()
+  result$other_cause_death_status=other_cause_death_time$status
+  result$other_cause_death_time=other_cause_death_time$time
   return(result)
   
 }  
@@ -110,10 +125,10 @@ sim_multiple_individuals_MCED_parallel_universe <- function(  cancer_sites,
                                          screen_interval,
                                          num_males, 
                                          num_females, 
-                                         all_fits_male, 
-                                         all_fits_female, 
-                                         all_meta_data_fits_female, 
-                                         all_meta_data_fits_male, 
+                                         all_rates_male, 
+                                         all_rates_female, 
+                                         all_meta_data_female, 
+                                         all_meta_data_male, 
                                          cdc_data, 
                                          hmd_data,
                                          MCED_cdc
@@ -127,26 +142,25 @@ sim_multiple_individuals_MCED_parallel_universe <- function(  cancer_sites,
   
   
   # Extract rate matrices matrices based on OMST and LMST specs (Male)
-  rates_list_male = get_rates_list(the_omst = OMST_vec, the_lmst = LMST_vec,
-                                   all_meta_data_fits = all_meta_data_fits_male,
-                                   all_fits = all_fits_male, the_cancer_site = cancer_sites)
-  sites_male = rates_list_male$cancer_sites %>% rename(the_cancer_site = cancer_site)
-  sites_male = sites_male$the_cancer_site
+  rates_list_male = get_filtered_rates(the_omsts = OMST_vec, the_lmsts = LMST_vec,
+                                   all_meta_data = all_meta_data_male,
+                                   all_rates = all_rates_male, the_cancer_site = cancer_sites)
+
+  sites_male = rates_list_male$cancer_sites 
   rates_list_male = rates_list_male$rates_list
-  
+
   # Extract rate matrices (Female)
-  rates_list_female = get_rates_list(the_omst = OMST_vec, the_lmst = LMST_vec, 
-                                     all_meta_data_fits = all_meta_data_fits_female, 
-                                     all_fits = all_fits_female, the_cancer_site = cancer_sites)
-  sites_female = rates_list_female$cancer_sites %>% rename(the_cancer_site = cancer_site)
-  sites_female = sites_female$the_cancer_site
+  rates_list_female = get_filtered_rates(the_omsts = OMST_vec, the_lmsts = LMST_vec, 
+                                     all_meta_data = all_meta_data_female, 
+                                     all_rates = all_rates_female, the_cancer_site = cancer_sites)
+  sites_female = rates_list_female$cancer_sites 
   rates_list_female = rates_list_female$rates_list                                    
   
   # Extract sensitivities and specificity based on selected cancer sites
   test_performance_male = test_performance_dataframe %>% filter(cancer_site %in% as.vector(sites_male))
   test_performance_female = test_performance_dataframe %>% filter(cancer_site %in% as.vector(sites_female))
   
-  
+ # browser()
   #Get the other-cause death tables for men and women
   other_cause_death_male=make_othercause_death_table(cdc_data=cdc_data,
                                                      MCED_cdc=MCED_cdc, 
@@ -191,7 +205,7 @@ sim_multiple_individuals_MCED_parallel_universe <- function(  cancer_sites,
   # Combine all individual results into one data frame
   combined_results_males <- do.call(rbind, results_list_male)
   combined_results_females <- do.call(rbind, results_list_female)
-  
+
   combined_results =bind_rows(combined_results_males,combined_results_females)%>%
                    mutate(start_age=starting_age)
   
