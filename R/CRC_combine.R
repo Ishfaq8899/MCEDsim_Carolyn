63c19c616df298054e9ee83c916248607449d930
Ishfaq Ahmad <ahmadish@ohsu.edu>
2025-10-28 12:08:45 -0700

    Modification

--- R/CRC_combine.R
+++ R/CRC_combine.R
@@ -1,11 +1,15 @@
 ###########################################################
 #' Simulate a cohort of individuals with and without Multicancer Early Detection (MCED) screening
 #'
+#' @description
 #' This function simulates cancer outcomes in a population with a designated starting age using a "parallel universe" approach.
 #' That is, cancer outcomes in the population are simulated with and without MCED screening.  The natural history (i.e., the times of cancer onset and clinical diagnosis) for 
 #' each individual is the same in both screening and no-screening scenarios.  
-#' The user specifies cancer sites in the MCED screening test.  The user also provides sensitivity of the tests for early and late-stage disease, where early refers to AJCC 7 stages I-II and late, III-IV, except for
-#' pancreas cancer, where early is stage I and late, II-IV. 
+#' 
+#' The user specifies cancer sites in the MCED screening test.  
+#' The user also provides sensitivity of the tests for early and late-stage disease, where early refers
+#' to AJCC 7 stages I-II and late, III-IV, except for pancreas cancer, where early is stage I and late, II-IV. 
+#'
 #' Natural history models are based on built-in fitted models that are calibrated to SEER 2015-2021 data by age and sex and can be specified based on 
 #' user-provided inputs about the overall mean sojourn time (OMST) and the late mean sojourn time (LMST) for each cancer site. 
 #' The function tracks only the first cancer diagnosis based on pre-clinical onset. 
@@ -16,14 +20,15 @@
 #' For both screened and unscreened individuals, cancer mortality is projected from the point of clinical diagnosis (i.e., post lead-time) to prevent
 #' lead-time bias.
 #'  
-#' @param cancer_sites Vector of cancer sites (allowable values include "Anus", ” Bladder" ,  ”Breast",  ”Colorectal" , "Esophagus"  , ”Gastric", "Headandneck", 
+#' @param cancer_sites Vector of cancer sites (allowable values include "Anus", ” Bladder" ,
+#'   ”Breast",  ”Colorectal" , "Esophagus"  , ”Gastric", "Headandneck", 
 #' "Liver" ,  "Lung",   "Pancreas", "Prostate", "Renal", "Uterine","Ovary")
-#' @param LMST_vec Vector of late mean sojourn times for each cancer site.
-#' @param OMST_vec Vector of overall mean sojourn times for each cancer site.
+#' @param LMST_vec Numeric vector of late mean sojourn times (years) for each cancer site.
+#' @param OMST_vec Numeric vector of overall mean sojourn times (years) for each cancer site.
 #' @param test_performance_dataframe Data frame with test sensitivity/specificity info.
 #' @param MCED_specificity Overall specificity for the MCED test (not specific to cancer site)
-#' @param starting_age Starting age for simulation.
-#' @param ending_age Ending age for simulation.
+#' @param starting_age Numeric starting age for simulation
+#' @param ending_age Numeric ending age for simulation
 #' @param num_screens Number of screening rounds.
 #' @param screen_interval Interval between screening rounds.
 #' @param num_males Number of male individuals to simulate.
@@ -35,7 +40,7 @@
 #' @param cdc_data CDC mortality data.
 #' @param hmd_data Human Mortality Database data.
 #' @param MCED_cdc CDC data for MCED.
-#' @param surv_param_table Survival parameters table.
+#' @param surv_param_table Data frame of cancer-specific survival parameters.
 #' @param CRC_data CRC data.
 #' @export
 #'
@@ -43,7 +48,7 @@
 #' The function returns each individual's first cancer site,  age and stage of clinical diagnosis in 
 #' absence of screening, age and stage at screen diagnosis, the time of other-cause mortality, and the time of cancer-specific mortality in absence
 #' and presence of screening.   Cancer diagnosis and death times are presented both with and without competing other-cause mortality.
-
+#'
 #' @examples
 #' library(MCEDsim)
 #' #Load the other-cause mortality tables
@@ -90,32 +95,32 @@
 #'                                                           surv_param_table=param_table,
 #'                                                           MCED_specificity = .995)
 sim_MCED_parallel_universe_before_CRC <- function(cancer_sites,
-                                                            LMST_vec, 
-                                                            OMST_vec, 
-                                                            test_performance_dataframe, 
-                                                            MCED_specificity,
-                                                            starting_age,
-                                                            ending_age,
-                                                            num_screens,
-                                                            screen_interval,
-                                                            num_males, 
-                                                            num_females, 
-                                                            all_rates_male, 
-                                                            all_rates_female, 
-                                                            all_meta_data_female, 
-                                                            all_meta_data_male, 
-                                                            cdc_data, 
-                                                            hmd_data,
-                                                            MCED_cdc,
-                                                            surv_param_table)
-{
+                                                  LMST_vec, 
+                                                  OMST_vec, 
+                                                  test_performance_dataframe, 
+                                                  MCED_specificity,
+                                                  starting_age,
+                                                  ending_age,
+                                                  num_screens,
+                                                  screen_interval,
+                                                  num_males, 
+                                                  num_females, 
+                                                  all_rates_male, 
+                                                  all_rates_female, 
+                                                  all_meta_data_female, 
+                                                  all_meta_data_male, 
+                                                  cdc_data, 
+                                                  hmd_data,
+                                                  MCED_cdc,
+                                                  surv_param_table){
   
-  #browser()
-  # Creat a vector of IDs
+  # Create a vector of IDs
   IDs_male <- 1:num_males
+  # Female IDs: continue sequentially after males
   IDs_female <-(num_males+1):(num_females+num_males)
   
   
+  # ---- Extract Sex-Specific Rate Matrices ----  
   # Extract rate matrices matrices based on OMST and LMST specs (Male)
   rates_list_male = get_filtered_rates(the_omsts = OMST_vec, the_lmsts = LMST_vec,
                                        all_meta_data = all_meta_data_male,
@@ -124,17 +129,19 @@ sim_MCED_parallel_universe_before_CRC <- function(cancer_sites,
   sites_male = rates_list_male$cancer_sites 
   rates_list_male = rates_list_male$rates_list
   
-  # Extract rate matrices (Female)
+  # Extract rate matrices matrices based on OMST and LMST specs (Female)
   rates_list_female = get_filtered_rates(the_omsts = OMST_vec, the_lmsts = LMST_vec, 
                                          all_meta_data = all_meta_data_female, 
                                          all_rates = all_rates_female, the_cancer_site = cancer_sites)
   sites_female = rates_list_female$cancer_sites 
-  rates_list_female = rates_list_female$rates_list                                    
+  rates_list_female = rates_list_female$rates_list
   
+  
+  # ---- Extract Test Performance Parameters ----
   # Extract sensitivities and specificity based on selected cancer sites
-  
   test_performance_male = test_performance_dataframe %>% filter(cancer_site %in% as.vector(sites_male))
   test_performance_female = test_performance_dataframe %>% filter(cancer_site %in% as.vector(sites_female))
+  
   
   #Get the other-cause death tables for men and women
   other_cause_death_male=make_othercause_death_table(cdc_data=cdc_data,
@@ -153,7 +160,8 @@ sim_MCED_parallel_universe_before_CRC <- function(cancer_sites,
                                                        selected_cancers=sites_female,
                                                        the_year=2018)
   
-  # Use mapply to apply the sim_individual_UKCTOCS function to each ID
+  # ---- Simulate Individual Outcomes ----
+  # Use mapply to apply the sim_individual_MCED function to each ID (males)
   results_list_male <- mapply(sim_individual_MCED,
                               ID = IDs_male,
                               MoreArgs = list(rates_list=rates_list_male,
@@ -168,6 +176,7 @@ sim_MCED_parallel_universe_before_CRC <- function(cancer_sites,
                                               sex="Male",MCED_specificity=MCED_specificity),
                               SIMPLIFY = FALSE)
   
+  # Use mapply to apply the sim_individual_MCED function to each ID (females)
   results_list_female <- mapply(sim_individual_MCED,
                                 ID = IDs_female,
                                 MoreArgs = list(rates_list=rates_list_female,
@@ -183,12 +192,15 @@ sim_MCED_parallel_universe_before_CRC <- function(cancer_sites,
                                                 MCED_specificity=MCED_specificity),
                                 SIMPLIFY = FALSE)
   
-  #get the first cancer and additional cancers for all individuals
+  
+  #Get the first cancer and additional cancers for all individuals (female)
   first_site_female=lapply(results_list_female,"[[","first_result")
   additional_sites_female=lapply(results_list_female,"[[","stored_result")
   
+  #Get the first cancer and additional cancers for all individuals (male)
   first_site_male=lapply(results_list_male,"[[","first_result")
   additional_sites_male=lapply(results_list_male,"[[","stored_result")
+  
   
   # Combine all individual results (first cancers) 
   combined_first_results_males <- do.call(rbind, first_site_male)%>%mutate(sex="Male")
@@ -202,14 +214,30 @@ sim_MCED_parallel_universe_before_CRC <- function(cancer_sites,
   combined_additional_results=bind_rows(combined_additional_results_males,combined_additional_results_females)%>%
     mutate(start_age=starting_age,end_time=ending_age)
   
-
- 
-  return(list(combined_additional_results=combined_additional_results,  combined_first_results=combined_first_results))
+  return(list(
+    combined_additional_results=combined_additional_results,  
+    combined_first_results=combined_first_results
+  ))
 }
-  
-  
 
-
+###########################################################
+#' Combine MCED Results with Colorectal Cancer Data
+#'
+#' @description
+#' This function integrates colorectal cancer (CRC) data with MCED simulation results, simulates cancer-specific survival
+#' for additional cancers diagnosed before other-cause death, and implements a reassignment algorithm to match additional
+#' cancers to individuals without primary cancer diagnoses based on age-at-death strata. The function processes competing 
+#' risks between cancer deaths, other-cause deaths, and censoring to calculate diagnosis and death outcomes in both screening
+#' and no-screening scenarios, while identifying overdiagnosis cases where cancers are screen-detected but would not have been
+#' clinically diagnosed before death from other causes.
+#'
+#' @param combined_first_results Data frame of first cancer diagnoses from function(sim_MCED_parallel_universe_before_CRC())
+#' @param combined_additional_results Data frame of additional cancer diagnoses
+#' @param starting_age Numeric starting age for cohort
+#' @param ending_age Numeric ending age for simulation
+#' @param CRC_data Data frame containing colorectal cancer simulation results
+#' @param surv_param_table Data frame of cancer-specific survival parameters
+#' 
 #' @export
 combine_MCED_CRC<- function(combined_first_results,
                             combined_additional_results,
@@ -218,32 +246,33 @@ combine_MCED_CRC<- function(combined_first_results,
                             CRC_data,
                             surv_param_table){
   
-
   #Note: consider if we want to use cancer onset or clinical diagnosis for additional cancers for purpose of calculating over diagnosis. 
   #If we decide this is important, change clinical_diagnosis_time to cancer_onset_time in the subsequent code
   
   #Filter additional cancers for those whose clinical diagnosis is prior to other cause death
   combined_additional_results <-combined_additional_results %>% filter(clinical_diagnosis_time <=other_cause_death_time)
   
-  #simulate cancer-specific deaths for additional cancers
+  
+  #Simulate cancer-specific deaths for additional cancers
   addtl_cancer_deaths=mapply(FUN="sim_cancer_deaths_screen_no_screen",clinical_diagnosis_time=combined_additional_results$clinical_diagnosis_time,
                              clinical_diagnosis_stage=combined_additional_results$clinical_diagnosis_stage,
                              cancer_site=combined_additional_results$cancer_site,
                              sex=combined_additional_results$sex,
                              ID=combined_additional_results$ID,
                              screen_diagnosis_stage=combined_additional_results$screen_diagnosis_stage,
-                             MoreArgs=list(surv_param_table=surv_param_table),SIMPLIFY=F)  
+                             MoreArgs=list(surv_param_table=surv_param_table),SIMPLIFY=F) 
   
   #Join cancer-specific deaths with cancer diagnoses for additional cancers
-  combined_additional_results=data.frame(do.call(rbind,addtl_cancer_deaths))%>%inner_join(combined_additional_results, by=c("ID","cancer_site"))
+  combined_additional_results = data.frame(do.call(rbind,addtl_cancer_deaths))%>%
+    inner_join(combined_additional_results, by=c("ID","cancer_site"))
   
-  #combine the CRC data with the additional cancers for reassignment.  CRC diagnoses that occur after other cause death do not 
-  #need to be reassigned so these people are removed from combined_additional_results.
-  combined_additional_results <- bind_rows(combined_additional_results, CRC_data)%>%
+  # Combine the CRC data with the additional cancers for reassignment.  CRC diagnoses that occur after other cause death do not 
+  #  need to be reassigned so these people are removed from combined_additional_results.
+  combined_additional_results <- bind_rows(CRC_data,combined_additional_results)%>%
     mutate(age_OC_death_cat=cut(other_cause_death_time,breaks=seq(0,150,by=5)))%>% 
     filter(clinical_diagnosis_time <=other_cause_death_time)
   
-  #Identify people who have clinical diagnosis of first cancer prior to other cause death  
+  # Identify people who have clinical diagnosis of first cancer prior to other cause death  
   primary_cancer <- combined_first_results %>% filter(clinical_diagnosis_time<=other_cause_death_time)
   
   #Identify people who do not have clinical diagnosis of first cancer prior to other cause death.  
@@ -253,39 +282,52 @@ combine_MCED_CRC<- function(combined_first_results,
     mutate(age_OC_death_cat=cut(other_cause_death_time,breaks=seq(0,150,by=5)))
   
   
-  #Counts of number of multiple cancers, primary cancers in lifeteime, and no primary cancers in lifetime
+  # Record counts for verification ----
+  # Counts of number of multiple cancers, primary cancers in lifetime, and no primary cancers in lifetime
   N_multiple_cancers=nrow(combined_additional_results)
   N_primary_cancers=nrow(primary_cancer)
   N_no_primary_cancer=nrow(no_primary_cancer)
   
-  #set index as ID to identify specific individual without primary cancer (used in bookkeeping in next step)
+  
+  
+  # Set index as ID to identify specific individual without primary cancer (used in bookkeeping in next step)
   no_primary_cancer <- no_primary_cancer %>% mutate(index=seq(1,nrow(no_primary_cancer)))
   
-  #This loop goes through the combined_additional_results rows and attempts to match in an individual in
-  #no_primary_cancer within the same OC death strata.  Then it adds the combined_additional_results row to primary_cancer
-  #and removes the matching row from no_primary cancer, since they are not eligible to matched again. 
-  #Finally, it removes the row from combined_additional_results and updates the no_match_counter.  
+  # ---- Reassignment Algorithm ----
+  # This loop goes through the combined_additional_results rows and attempts to match in an individual in
+  #   no_primary_cancer within the same OC death strata. 
+  # Then it adds the combined_additional_results row to primary_cancer and removes the matching row from
+  #   no_primary cancer, since they are not eligible to matched again. 
+  # Finally, it removes the row from combined_additional_results and updates the no_match_counter.  
   no_match_counter=0
   while(nrow(combined_additional_results)>0){
-    test=match_individual(i=1,combined_additional_results=combined_additional_results, no_primary_cancer=no_primary_cancer)
-    
+    # Attempt to match first row of additional cancers
+    test = match_individual(
+      i=1,
+      combined_additional_results = combined_additional_results,
+      no_primary_cancer = no_primary_cancer
+    )
+    # If match found
     if(length(test)>1){
+      # Add this additional cancer to primary_cancer
       primary_cancer=bind_rows(combined_additional_results[1,],primary_cancer)
       no_primary_cancer=no_primary_cancer%>%filter(index!=test$index)
     }else{
-      no_match_counter=no_match_counter+1
+      # No match found, increment counter
+      no_match_counter = no_match_counter+1
     }
+    # Remove processed row
     combined_additional_results=combined_additional_results[-1,]
     
   }
   
-  
-  #check to see if the added and subtracted rows match expectations based on previous loop. 
+  # ---- Verify counts ----
+  # Check to see if the added and subtracted rows match expectations based on previous loop. 
   N_multiple_cancers_2=nrow(combined_additional_results)
   N_primary_cancers_2=nrow(primary_cancer)
   N_no_primary_cancer_2=nrow(no_primary_cancer)
   
-  #Final data with all cancers combined (first cancers and reassigned cancers)
+  # Final data with all cancers combined (first cancers and reassigned cancers)
   combined_results=bind_rows(primary_cancer,no_primary_cancer) 
   
   #Process data with other cause death as a censoring event
@@ -344,5 +386,5 @@ combine_MCED_CRC<- function(combined_first_results,
                                                
   )
   
-  return(list(results = combined_results,no_match_counter = no_match_counter))
+  return(list(results = combined_results, no_match_counter = no_match_counter))
 }
